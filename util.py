37b6ea91225d43826bdf7b0d7bc3fb7c973e6b93
Van Pham <vanhongpham01@gmail.com>
2021-11-14 19:48:29 -0500

    rename to util

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from authenticate import authenticate
 import util_help as uh
 
- 
+
 token=authenticate("eyyolk","user-library-read","409f2b568b5146298d8db0ff754ce726","c85512ab0ea94debae3c39f5adae1c44","https://www.facebook.com/hiiamvan")
 
 ngot = '57g2v7gJZepcwsuwssIfZs'
